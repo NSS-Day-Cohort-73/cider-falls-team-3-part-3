355ce79c3beed258d0a9d4535350a53d6f9b3c35
Niles Clancy <nilesclancy@gmail.com>
2024-10-01 14:38:55 -0400

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-import { renderDestinations } from './destination.js'
-import { renderParkInfo } from './parkInfo.js'
-import { renderGuests } from './guests.js'
+// import { renderDestinations } from './destination.js'
+// import { renderParkInfo } from './parkInfo.js'
+// import { renderGuests } from './guests.js'
 
-renderDestinations()
-renderParkInfo()
-renderGuests()
+// renderDestinations()
+// renderParkInfo()
+// renderGuests()
